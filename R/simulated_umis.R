9f2a82211c6a013d4920f2adfe3790e04b7517c1
Felix <felixwertek@gmail.com>
2021-05-29 23:13:52 +0200

    simulated_umis

---
+++ R/simulated_umis.R
@@ -0,0 +1,19 @@
+# Documents the data set generated in data-raw/simulated_umis.R
+# (see source code of cellpypes package).
+
+
+
+#' Simulated scRNAseq data
+#'
+#' This data serves to develop cellpypes and to illustrate its functionality.
+#' I made it up entirely.
+#'
+#' @format A list with 4 entries:
+#' \describe{
+#'   \item{raw}{Raw (unnormalized) UMI counts for a handful of genes, last columns are totalUMI. }
+#'   \item{neighbors}{Indices of each cell's 50 nearest neighbors.}
+#'   \item{embed}{Simulated UMAP embedding.}
+#'   \item{celltype}{Cell type label that I used to simulate the data.}
+#' }
+#' @source Very simple simulation (c.f. data-raw/simulated_umis.R in source code).
+"simulated_umis"
---
+++ data-raw/simulated_umis.R
@@ -0,0 +1,44 @@
+## code to prepare `simulated_umis` dataset goes here
+library(Matrix)
+
+
+# Simulate 3 cell types with different numbers of cells:
+n1 = 600
+n2 = 200
+n3 = 1100
+# features:
+set.seed(42)
+raw <- data.frame(
+  CD3E = rpois(n1+n2+n3, lambda=c(rep(1,n1), rep(1,n2), rep(.1,n3))),
+  FOXP3= rpois(n1+n2+n3, lambda=c(rep(.001,n1), rep(.5,n2), rep(.001,n3))),
+  ICOS = rpois(n1+n2+n3, lambda=c(rep(3,n1), rep(c(3, .1),n2/2), rep(.1,n3))),
+  MS4A1= rpois(n1+n2+n3, lambda=c(rep(.1,n1), rep(.1,n2), rep(1,n3))),
+  # gene = rpois(n1+n2+n3, lambda=c(rep(1,n1), rep(1,n2), rep(1,n3)))
+  totalUMI = round(rlnorm(n1+n2+n3, log(1800), .25))
+)
+raw <- as(as.matrix(raw), "dgCMatrix")
+# with few genes, uwot returns strange embeddings, so I simulate by hand:
+set.seed(42)
+ump <- data.frame(
+  u1 = rnorm(n1+n2+n3, mean = c(rep(-1,n1), rep(c(.5,1.5),n2/2), rep(8,n3)), sd=.5),
+  u2 = rnorm(n1+n2+n3, mean = c(rep(5,n1), rep(3.5,n2), rep(1,n3)), sd=.25),
+  celltype= c(rep("Th",n1), rep("Treg",n2), rep("B",n3))
+)
+
+ump %>% ggplot(aes(u1,u2, col=celltype))+geom_point()+ coord_fixed()
+scUtils::feat(ump[,1:2], raw[, "FOXP3"]/raw[,"totalUMI"])
+scUtils::feat(ump[,1:2], raw[, "ICOS"]/raw[,"totalUMI"])
+
+nn <- scpr::get.knn.annoy(as.matrix(sqrt(raw[,-ncol(raw)] / raw[, "totalUMI"])))
+
+simulated_umis <- list(
+  raw=raw,
+  neighbors=nn$idx,
+  embed    = ump[,1:2],
+  celltype = ump$celltype
+)
+
+usethis::use_data(simulated_umis, overwrite = TRUE)
+
+# gives weird embedding, I'm too lazy to tweak UMAP so I don't pursue this further:
+# ump <- uwot::umap(sqrt(raw[,-ncol(raw)] / raw[, "totalUMI"]))
