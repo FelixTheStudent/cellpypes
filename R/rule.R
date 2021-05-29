699151a6189f544c2db873d0cf8aa613d8480bc0
Felix <felixwertek@gmail.com>
2021-05-30 01:44:03 +0200

    rule is finished but untested

--- R/rule.R
+++ R/rule.R
@@ -1,14 +1,48 @@
 
 
+#' Check if obj$classes looks as expected.
+#' is_class returns FALSE for example in these cases:
+#'   is_classes(NULL)
+#'   is_classes(data.frame())
+#'   is_classes(data.frame(class=c("T","T"), parent=c("..root..","..root..")))
+#' @param classes 
+#'
+#' @return
+#'
+#' @examples
+is_classes <- function(classes) {
+    inherits(classes, "data.frame"  ) &&
+    ncol(classes) > 0 &&  # handles this: `is_classes(data.frame())`
+    all(colnames(classes)[1:2] == c("class", "parent")) &&
+    all(!duplicated(classes$class))
+  }
 
-#' Add 
+#' Check if obj$rules looks as expected.
+#'
+#' @param rules 
+#'
+#' @return
+#'
+#' @examples
+is_rules <- function(rules) {
+    inherits(rules, "data.frame"  ) &&
+    ncol(rules) > 0 &&  # handles this: `is_rules(data.frame())`
+    all(colnames(rules)[1:4] == c("class","feature","operator","threshold")) &&
+    all(!duplicated(paste0(rules$class, rules$feature)))
+  }
+
+
+
+#' Add a cell type rule.
 #'
 #' @param obj 
 #' @param class 
 #' @param feature 
-#' @param operator 
+#' @param operator One of the following: \code{c("<", ">", "<=", ">=")}.
 #' @param t 
-#' @param parent 
+#' @param parent The parent class (e.g. "T" or "T cell").
+#' If NULL, new classes get "..root.." and
+#' existing classes keep their current parent.
 #'
 #' @return
 #' @export
@@ -20,14 +54,55 @@ rule <- function(obj,
                  operator = ">",
                  threshold,
                  parent = NULL) {
+  # check inputs:
   if( any(is.null(obj), is.null(class), is.null(feature), is.null(threshold) )) { 
     stop("rule does not accept NULL input for arguments: obj, class, feature and threshold.", call. = F) }
   stopifnot(all(
     is_scalar_character(class), is_scalar_character(feature),
     is_scalar_character(operator), is_scalar_character(parent)|is.null(parent),
     is_scalar_double(threshold) ) )
+  stopifnot(operator %in% c("<", ">", "<=", ">="))
   if(any(parent==class)) stop("Class and parent cannot be the same.")
+  if(any(parent=="..root..")) stop("Cellpypes internally uses '..root..'. Call your parent something else!")
 
+  # check that obj has everything this rule needs:
+  stopifnot(feature %in% colnames(obj$raw))
+  stopifnot(is.null(obj$classes) || is_classes(obj$classes) )
+  stopifnot(is.null(obj$rules) || is_rules(obj$rules) )
+  stopifnot(is.null(obj$classes) == is.null(obj$rules))
+  
+  # is_classes also checks if obj$class is NULL
+  existing_class <- ifelse(is_classes(obj$class) && class %in% obj$class,
+                           TRUE,
+                           FALSE)
+  existing_feature <- ifelse(is_rules(obj$rules) && class %in% obj$rules$class &&
+                               feature %in% obj$rules$feature,
+                             TRUE,
+                             FALSE)
+  
+  # design choices:
+  #  * parent=NULL means "use existing parent"
+  #  * top classes have parent="..root..":
+  if (is.null(parent)) {
+    parent <- ifelse(existing_class,
+                     obj$classes$parent[obj$classes==class],
+                     "..root..")
+  }
+  
+  class_dat <- data.frame(class=class, parent=parent)
+  rule_dat  <- data.frame(class=class, feature=feature, 
+                          operator=operator, threshold=threshold)
+  # save to object
+  if (existing_class) {
+    obj$classes[obj$classes$class==class,] <- class_dat
+  } else {
+    obj$classes <- rbind(class_dat, obj$classes)
+  }
+  if (existing_feature) {
+    obj$rules[obj$rules$class==class && obj$rules$feature==feature,] <- rule_dat
+  } else {
+    obj$rules <- rbind(rule_dat, rules) # newest rule is on top
+  }
   
   
   
