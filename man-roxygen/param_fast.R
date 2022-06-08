b0185369f70021cebb3ce1b68bca4a685bf1b232
Felix Frauhammer <felixwertek@gmail.com>
2022-06-08 10:56:49 +0200

    docu updates

--- man-roxygen/param_fast.R
+++ man-roxygen/param_fast.R
@@ -1,3 +1,4 @@
 #' @param fast Set this to TRUE if you want fast plotting in spite of many cells
 #' (using the scattermore package). If NULL (default), cellpypes decides
-#' automatically and fast plotting is done for more than 10k cells.
+#' automatically and fast plotting is done for more than 10k cells, if FALSE
+#' it always uses `geom_point`.
