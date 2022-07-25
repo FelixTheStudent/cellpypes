8b333f320aa35f6e70f2dac0968481e23b45648a
Felix Frauhammer <felixwertek@gmail.com>
2022-07-25 21:16:44 +0200

    knn_refine docu update

--- man-roxygen/knn_refine.R
+++ man-roxygen/knn_refine.R
@@ -3,14 +3,15 @@
 #' 
 #' By default, cellpypes only assigns cells to a class if all relevant rules
 #' apply.
-#' In other words, marker gene UMI counts in the cell's neighborhood all have to 
+#' In other words, all marker gene UMI counts in the cell's neighborhood all have to 
 #' be clearly above/below their threshold.
 #' Since UMI counts are sparse (even after neighbor pooling done by cellpypes),
 #' this can leave many cells unassigned.
 #' 
-#' It is reasonable to assume a unassigned cell is of the same cell type as the
+#' It is reasonable to assume an unassigned cell is of the same cell type as the
 #' majority of its nearest neighbors.
-#' Therefore, cellpypes implements a kNN classifier to refine the labels from
+#' Therefore, cellpypes implements a kNN classifier to further refine labels 
+#' obtained by 
 #' manually thresholding UMI counts.
 #' `knn_refine = 0.3` means a cell is assigned the class label held by
 #' most of its neighbors unless no class gets more than 30 %.
@@ -19,6 +20,8 @@
 #' (which assigns all cells),
 #' while `knn_refine = 0.5` leaves cells 'in between' two similar
 #' cell types unassigned.
+#' 
+#' 
 #' We recommend looking at `knn_refine =  0` first as it's faster and
 #' more directly tied to marker gene expression.
 #' If assigning all cells is desired, we recommend `knn_refine = 0.3` or lower,
